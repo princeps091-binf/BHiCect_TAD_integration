3551c02075ed34482d2cbaec63d27ac8a1c5d6c8
princeps091-binf <princeps091@gmail.com>
2022-04-04 19:00:01 +0200

    integration of CAGE-rich TADs when examining recovery

--- code/TAD_intersect_explore.R
+++ code/TAD_intersect_explore.R
@@ -13,13 +13,24 @@ get_tbl_in_fn<-function(tmp_file){
 }
 #--------------------------------
 inter_file<-"./data/intersection_out_tbl/HMEC_TAD_cl_intersect.Rda"
+TAD_file<-"~/Documents/multires_bhicect/Bootstrapp_fn/data/pval_tbl/CAGE_union_HMEC_TAD_pval_tbl.Rda"
 
+TAD_tbl<-get_tbl_in_fn(TAD_file)%>% 
+  group_by(chr) %>% 
+  mutate(FDR=p.adjust(emp.pval,method="fdr")) %>% 
+  filter(FDR<=0.01)
 
 TAD_cl_inter_tbl<-get_tbl_in_fn(inter_file)
 
 TAD_cl_inter_tbl %>% 
   ggplot(.,aes(max.inter))+
   geom_histogram()
+
+TAD_cl_inter_tbl %>%
+  mutate(ID=paste(chr,start,end,sep="_")) %>% 
+  mutate(cage.rich=ifelse(ID %in% TAD_tbl$ID,"rich",'other')) %>% 
+  ggplot(.,aes(max.inter,color=cage.rich))+
+  geom_density()
 
 
 TAD_cl_inter_tbl<-TAD_cl_inter_tbl %>% 
@@ -34,3 +45,10 @@ cl_cover<-unlist(lapply(width(IRanges::intersect(TAD_Grange_l,max_cl_GRange_l)),
 tibble(tad=tad_cover,cl=cl_cover) %>% 
   ggplot(.,aes(tad,cl))+
   geom_density_2d_filled()
+
+TAD_cl_inter_tbl %>% 
+  mutate(ID=paste(chr,start,end,sep="_")) %>% 
+  mutate(tad.cover=tad_cover,cl.cover=cl_cover) %>% 
+  mutate(cage.rich=ifelse(ID %in% TAD_tbl$ID,"rich",'other')) %>% 
+  ggplot(.,aes(cl.cover,color=cage.rich))+
+  geom_density()
