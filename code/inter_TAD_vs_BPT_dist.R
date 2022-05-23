159b17f71d3d3849fb27ea730d527a49a2f55c90
princeps091-binf <princeps091@gmail.com>
2022-05-23 10:32:59 +0200

    adjusted the inter-TAD analysis to process genome-wide

--- code/inter_TAD_vs_BPT_dist.R
+++ code/inter_TAD_vs_BPT_dist.R
@@ -36,13 +36,13 @@ compute_chr_res_zscore_fn<-function(dat_file,cl_res,chromo,res_num){
 }  
 
 #--------------------------------
-inter_file<-"./data/intersection_out_tbl/GM12878_top_TAD_cl_intersect.Rda"
-res_file<-"~/Documents/multires_bhicect/data/GM12878/spec_res/"
-HiC_dat_folder<-"~/Documents/multires_bhicect/data/GM12878/"
+# inter_file<-"./data/intersection_out_tbl/GM12878_top_TAD_cl_intersect.Rda"
+# res_file<-"~/Documents/multires_bhicect/data/GM12878/spec_res/"
+# HiC_dat_folder<-"~/Documents/multires_bhicect/data/GM12878/"
 
-#inter_file<-"~/data_transfer/GM12878_top_TAD_cl_intersect.Rda"
-#res_file<-"/storage/mathelierarea/processed/vipin/group/HiC_data/GM12878/spec_res/"
-#HiC_dat_folder<-"/storage/mathelierarea/processed/vipin/group/HiC_data/GM12878/"
+inter_file<-"~/data_transfer/GM12878_top_TAD_cl_intersect.Rda"
+res_file<-"/storage/mathelierarea/processed/vipin/group/HiC_data/GM12878/spec_res/"
+HiC_dat_folder<-"/storage/mathelierarea/processed/vipin/group/HiC_data/GM12878/"
 
 TAD_cl_inter_tbl<-get_tbl_in_fn(inter_file)
 
@@ -50,6 +50,105 @@ TAD_best_cl_fit_tbl<-TAD_cl_inter_tbl %>%
   dplyr::select(chr,start,end,GRange,max.inter,max.cl)
 rm(TAD_cl_inter_tbl)
 
+tmp_res<-"50kb"
+chr_set<-grep("^chr",str_split_fixed(list.files(res_file),"_",2)[,1],value=T)
+
+chr_res_l<-vector("list",length(chr_set))
+names(chr_res_l)<-chr_set
+
+for(chromo in chr_set){
+  message(chromo)
+  chr_TAD_best_cl_fit_tbl<-TAD_best_cl_fit_tbl %>% 
+    filter(chr==chromo)
+  chr_TAD_best_cl_fit_tbl<-chr_TAD_best_cl_fit_tbl %>% 
+    mutate(TAD.ID=paste(chr,start,end,sep="_"))
+  
+  message("compute BPT distance for ", chromo)
+  chr_spec_res<-get_tbl_in_fn(paste0(res_file,chromo,"_spec_res.Rda"))
+  chr_bpt<-FromListSimple(chr_spec_res$part_tree)
+  node_ancestor<-chr_bpt$Get(function(x){x$Get('name',traversal='ancestor')})
+  node_ancestor<-lapply(node_ancestor,'[',-1)
+  
+  TAD_node<-unique(chr_TAD_best_cl_fit_tbl%>%dplyr::select(max.cl) %>% unlist)
+  TAD_set<-unique(c(TAD_node,unique(unlist(node_ancestor[TAD_node])))) 
+  Prune(chr_bpt, function(x) x$name %in% TAD_set)
+  
+  g_bpt<-as.igraph.Node(chr_bpt,directed = T,direction = 'climb')
+  
+  tmp_d<-distances(g_bpt,TAD_node,TAD_node)
+  
+  
+  message("compute zscore for ", chromo)
+  chr_dat<-compute_chr_res_zscore_fn(HiC_dat_folder,tmp_res,chromo,res_num)
+  tmp_bins<-unique(c(chr_dat$X1,chr_dat$X2))
+  
+  chr_bin_GRange<-GRanges(seqnames=chromo,
+                          ranges = IRanges(start=tmp_bins,
+                                           end=tmp_bins + res_num[tmp_res] -1
+                          ))
+  TAD_ID<-chr_TAD_best_cl_fit_tbl$TAD.ID
+  top_TAD_GRangeL<-GRangesList(chr_TAD_best_cl_fit_tbl$GRange)
+  
+  bin_TAD_inter_tbl<-findOverlaps(top_TAD_GRangeL,chr_bin_GRange) %>% 
+    as_tibble %>% 
+    mutate(TAD.ID=TAD_ID[queryHits],bin=tmp_bins[subjectHits]) 
+  
+  
+  intersect_size<-unlist(lapply(pintersect(top_TAD_GRangeL[bin_TAD_inter_tbl$queryHits],chr_bin_GRange[bin_TAD_inter_tbl$subjectHits]),width))
+  
+  bin_TAD_inter_tbl<-bin_TAD_inter_tbl %>% 
+    mutate(inter.size=intersect_size)
+  
+  chr_TAD_bin_content<-bin_TAD_inter_tbl %>% 
+    filter(inter.size>=res_num[tmp_res]) %>% 
+    group_by(TAD.ID) %>% 
+    summarise(bin.content=list(unique(bin)))
+  
+  TAD_pair_combo<-t(combn(chr_TAD_bin_content$TAD.ID,2))
+  
+  inter_TAD_hic_dat<-chr_dat %>% 
+    filter(X1 %in% unique(unlist(chr_TAD_bin_content$bin.content)) & X2 %in% unique(unlist(chr_TAD_bin_content$bin.content)))
+  
+  message("Subset inter-TAD interaction for", chromo)
+  
+  plan(multisession,workers=15)
+  inter_TAD_hic_dat_l<-future_map(1:nrow(TAD_pair_combo),function(i){
+    
+    bins_a<-chr_TAD_bin_content %>% 
+      filter(TAD.ID==TAD_pair_combo[i,1]) %>% 
+      unnest(cols=c(bin.content)) %>% 
+      dplyr::select(bin.content) %>% unlist
+    
+    bins_b<-chr_TAD_bin_content %>% 
+      filter(TAD.ID==TAD_pair_combo[i,2]) %>% 
+      unnest(cols=c(bin.content)) %>% 
+      dplyr::select(bin.content) %>% unlist
+    
+    return(inter_TAD_hic_dat %>% 
+             filter(X1 %in% bins_a & X2 %in% bins_b | X1 %in% bins_b & X2 %in% bins_a) %>% 
+             dplyr::select(X1,X2,X3,zscore))
+    
+  })
+  plan(sequential)
+  
+  best_cl_vec<-chr_TAD_best_cl_fit_tbl$max.cl
+  names(best_cl_vec)<-chr_TAD_best_cl_fit_tbl$TAD.ID
+  
+  TAD_pair_combo_tbl<-TAD_pair_combo %>% 
+    as_tibble %>% 
+    mutate(cl.A=best_cl_vec[TAD_pair_combo[,1]],cl.B=best_cl_vec[TAD_pair_combo[,2]])
+  
+  TAD_pair_combo_tbl<-TAD_pair_combo_tbl %>% 
+    mutate(bpt.d=tmp_d[as.matrix(TAD_pair_combo_tbl[,3:4])])
+  
+  TAD_pair_combo_tbl<-TAD_pair_combo_tbl %>% 
+    mutate(med.z=inter_TAD_hic_dat_l)
+  
+  chre_res_l[[chromo]]<-TAD_pair_combo_tbl
+}
+
+
+#-------------------------------------------------------------------------
 chromo<-"chr22"
 
 chr_TAD_best_cl_fit_tbl<-TAD_best_cl_fit_tbl %>% 
@@ -70,11 +169,6 @@ g_bpt<-as.igraph.Node(chr_bpt,directed = T,direction = 'climb')
 
 tmp_d<-distances(g_bpt,TAD_node,TAD_node)
 
-
-
-tmp_res_set<-c("50kb","10kb","5kb")
-
-tmp_res<-"50kb"
 
 chr_dat<-compute_chr_res_zscore_fn(HiC_dat_folder,tmp_res,chromo,res_num)
 tmp_bins<-unique(c(chr_dat$X1,chr_dat$X2))
@@ -105,7 +199,7 @@ TAD_pair_combo<-t(combn(chr_TAD_bin_content$TAD.ID,2))
 inter_TAD_hic_dat<-chr_dat %>% 
   filter(X1 %in% unique(unlist(chr_TAD_bin_content$bin.content)) & X2 %in% unique(unlist(chr_TAD_bin_content$bin.content)))
 
-plan(multisession,workers=4)
+plan(multisession,workers=15)
 inter_TAD_hic_dat_l<-future_map(1:nrow(TAD_pair_combo),function(i){
   
   bins_a<-chr_TAD_bin_content %>% 
